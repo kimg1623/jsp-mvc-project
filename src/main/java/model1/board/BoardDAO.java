80c32cdb82fbdd1476b2c3c4dd0153392377f6a3
Jiwon <kimg1623@naver.com>
2023-11-14 13:39:13 +0900

    Add BoardDAO.java

---
+++ src/main/java/model1/board/BoardDAO.java
@@ -0,0 +1,72 @@
+package model1.board;
+
+import common.JDBConnect;
+
+import javax.servlet.ServletContext;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+public class BoardDAO extends JDBConnect {
+    public BoardDAO(ServletContext application){
+        super(application);
+    }
+
+    // 검색 조건에 맞는 게시물의 개수를 반환
+    public int selectCount(Map<String, Object> map){
+        int totalCount = 0; // 결과(게시물 수)를 담을 변수
+
+        // 게시물 수를 얻어오는 쿼리문 작성
+        String query = "SELECT COUNT(*) FROM board";
+        if(map.get("searchWord") != null){
+            query += " WHERE " + map.get("searchField") + " "
+                    + " LIKE  '%" + map.get("searchWord") + "%'";
+        }
+
+        try{
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+            rs.next(); // 커서를 첫 번째 행으로 이동
+            totalCount = rs.getInt(1); // 첫 번째 컬럼 값을 가져옴
+        } catch (Exception e) {
+            System.out.println("게시물 수를 구하는 중 예외 발생");
+            e.printStackTrace();
+        }
+
+        return totalCount;
+    }
+
+    public List<BoardDTO> selectList(Map<String, Object> map){
+        List<BoardDTO> bbs = new Vector<>();
+
+        String query = "SELECT * FROM board";
+        if(map.get("searchWord") != null){
+            query += " WHERE " + map.get("searchField") + " "
+                    + " LIKE  '%" + map.get("searchWord") + "%'";
+        }
+        query += " ORDER BY num DESC ";
+
+        try{
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+            while(rs.next()){
+                // 한 행(게시물 하나)의 내용을 DTO에 저장
+                BoardDTO dto = new BoardDTO();
+
+                dto.setNum(rs.getString("num"));
+                dto.setTitle(rs.getString("title"));
+                dto.setContent(rs.getString("content"));
+                dto.setPostdate(rs.getDate("postdate"));
+                dto.setId(rs.getString("id"));
+                dto.setVisitcount(rs.getString("visitcount"));
+
+                bbs.add(dto);
+            }
+        } catch (Exception e) {
+            System.out.println("게시물 조회 중 예외 발생");
+            e.printStackTrace();
+        }
+
+        return bbs;
+    }
+}
